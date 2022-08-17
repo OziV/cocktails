923e5d8d3df2572666b086ee9aa7fc7282b3cb93
OziV <oz.ziv8@gmail.com>
2022-08-17 18:17:32 +0300

    WIP: adding pages & components

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useGlobalContext } from "./context";
 
 function Home() {
-  return <div>Home</div>
+  const data = useGlobalContext();
+  console.log(data);
+  return (
+    <>
+      <div>Home</div>
+    </>
+  );
 }
 
 export default Home;
---
+++ src/context.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect, useContext } from "react";
+
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  return (
+    <AppContext.Provider value="Hello from context">
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+// Custom Hook
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
