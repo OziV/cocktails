ac496ffb65ec4599bf0f49f413fedb6297435bbd
OziV <oz.ziv8@gmail.com>
2022-08-17 19:24:52 +0300

    WIP: adding about & error pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
-import Home from "./Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCocktail from "./pages/SingleCocktail";
+import Error from "./pages/Error";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="cocktail/:id" element={<SingleCocktail />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,17 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+        repudiandae iste quas esse asperiores tenetur, voluptatibus facere illo
+        veniam illum reiciendis, at eligendi dolores ea nemo consequuntur
+        sapiente officia placeat, magnam officiis laboriosam voluptate quasi
+        vero. Alias a animi dolore quisquam beatae laborum. Rem sequi dicta cum
+        veritatis eveniet alias.
+      </p>
+    </section>
   );
 };
 
