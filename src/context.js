51c6534195dd21333d74518451a76d318c63ea3c
OziV <oz.ziv8@gmail.com>
2022-08-18 16:40:50 +0300

    FIX: fix context

--- src/context.js
+++ src/context.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect, useContext, useCallback } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import { useCallback } from "react";
 
 const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
-
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState("a");
   const [cocktails, setCocktails] = useState([]);
 
@@ -14,11 +14,13 @@ const AppProvider = ({ children }) => {
     try {
       const response = await fetch(`${url}${searchTerm}`);
       const data = await response.json();
+      console.log(data);
       const { drinks } = data;
       if (drinks) {
         const newCocktails = drinks.map((item) => {
           const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
             item;
+
           return {
             id: idDrink,
             name: strDrink,
@@ -36,20 +38,19 @@ const AppProvider = ({ children }) => {
       console.log(error);
       setLoading(false);
     }
-  }, [setSearchTerm]);
-
+  }, [searchTerm]);
   useEffect(() => {
     fetchDrinks();
   }, [searchTerm, fetchDrinks]);
-
   return (
-    <AppContext.Provider value={{ loading, cocktails, setSearchTerm }}>
+    <AppContext.Provider
+      value={{ loading, cocktails, searchTerm, setSearchTerm }}
+    >
       {children}
     </AppContext.Provider>
   );
 };
-
-// Custom Hook
+// make sure use
 export const useGlobalContext = () => {
   return useContext(AppContext);
 };
